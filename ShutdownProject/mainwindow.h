d9554c00f553b206776f64204b2e4ca7cb4e3608
Nathan Ahrens <nathanmahrens@gmail.com>
2017-09-29 14:53:16 -0400

    Adding include statements and adding pointers

--- ShutdownProject/mainwindow.h
+++ ShutdownProject/mainwindow.h
@@ -1,15 +1,34 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+/*
+ * Shutdown Interface
+ * Nathan Ahrens
+ * Sept. 29 2017
+ *
+ * This program serves as a program to allow the user to graphically select when to shutdown the computer.
+ */
+
 #include <QMainWindow>
+#include <QWidget>
+#include <QPushButton>
+#include <QAction>
+#include <QLabel>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    QWidget *centralWidget;
 
 public:
-    MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private:
+    QPushButton *shutdownButton;
+    QPushButton *cancelButton;
+    QPushButton *exitButton;
+
 };
 
 #endif // MAINWINDOW_H
